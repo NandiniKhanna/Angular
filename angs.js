21f97a4142eb75a16317603aba83ac6a7b11e359
NandiniKhanna <1997nandinikhanna@gmail.com>
2017-05-16 20:42:55 +0530

    Timeout

--- angs.js
+++ angs.js
@@ -1,6 +1,12 @@
 var app = angular.module("myApp", []);
 app.directive("w3TestDirective", function() {
     return {
-        template : "<h1>HELLO WORLD</h1>"
+        template : "<h1>MESSAGE FROM DIRECTIVE</h1>"
     };
+});
+app.controller('myCtrl', function($scope, $timeout) {
+  $scope.myHeader = "Hello World!";
+  $timeout(function () {
+      $scope.myHeader = "How are you today?";
+  }, 2000);
 });
